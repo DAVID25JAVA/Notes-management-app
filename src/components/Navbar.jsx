2edfca8e492a2ae2da483131f5b0d706c13c0958
David <dp2072000@gmail.com>
2026-01-08 20:36:28 +0530

    all done with functionality complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,14 @@ function Navbar() {
       <div className="max-w-8xl mx-auto px-4 sm:px-5 md:px-5 py-2">
         <div className="flex justify-between items-center">
           <Link href={"/"}>
-            <div className="cursor-pointer">
+            <div className="cursor-pointer flex items-center gap-2 font-semibold text-base sm:text-lg">
               <Image
                 alt="logo"
                 src={assets?.note}
                 width={40}
                 className="border p-1 rounded-full border-indigo-300"
               />
+              <span>Notes Management App</span>
             </div>
           </Link>
         </div>
